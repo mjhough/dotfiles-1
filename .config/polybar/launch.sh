dea13d65bf846b18615e08a480a10fbf48e88970
Sean <sean_ling@kiipo.com>
2019-01-31 00:25:12 -0500

    polybar launch

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,10 +5,19 @@ killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-if xrandr | grep "HDMI-0 connected"
-  then polybar top_primary & polybar top_secondary & polybar bottom_primary & polybar bottom_secondary
-  else polybar top_single & polybar bottom_single
+if [[ $(hostname) == 'sean-pavilion' ]]; then
+  if xrandr | grep "HDMI-0 connected"
+    then polybar top_primary & 
+         polybar top_secondary & 
+         polybar bottom_primary & 
+         polybar bottom_secondary
+    else polybar top_single & 
+         polybar bottom_single
+  fi
+elif [[ $(hostname) == 'sean-desktop' ]]; then
+  polybar top_desktop_primary & 
+  polybar top_desktop_secondary & 
+  polybar bottom_desktop_primary & 
+  polybar bottom_desktop_secondary
 fi
-
 echo "Polybar Launched"
